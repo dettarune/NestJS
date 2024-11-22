eb1ed6f3a31b218696ca55e236b0608da6b69470
detarune <learnbygoogling@gmail.com>
2024-11-23 05:02:51 +0700

    nest

---
+++ src/user/user/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Post, Get, Req, Param, Query, Header, HttpStatus, HttpCode } from '@nestjs/common';
+import { Request } from 'express';
+
+
+@Controller('/api/users')
+export class UserController {
+
+    @Get('/hello')
+    @Header("Content-type", "application/json")
+    @HttpCode(200)
+    getById(): Record<string, string>{
+        return {
+            data: "Hello Eko"
+        }
+    }
+}
